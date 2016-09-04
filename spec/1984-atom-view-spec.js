733222a31f83761246ec0f5f390f0f9ce3bf2f44
Daniel Parkin <danielparkin1@gmail.com>
2016-09-04 23:08:29 +0100

    initial commit

---
+++ spec/1984-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import 1984AtomView from '../lib/1984-atom-view';
+
+describe('1984AtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
